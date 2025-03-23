5acbdab760c202077af5e0cadacbe762df5f84e2
shobhadpr <136736112+shobhadpr@users.noreply.github.com>
2025-03-24 00:24:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetipsforrrryou" }');
+    res.send('{ "response": "Hello From Thetipsforrrryou--Gowridevi-shankar-haramahadev" }');
 });
 
 app.get('/will', function (req, res) {
